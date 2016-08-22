629330eafac6f02693657960bbfe0d35c37b1cae
Sonar Systems <SonarSystems@users.noreply.github.com>
2016-08-22 13:35:27 +0100

    Update main.cpp

--- [SETUP]/GLFW/Mac/OGL/main.cpp
+++ [SETUP]/GLFW/Mac/OGL/main.cpp
@@ -26,6 +26,9 @@ int main( )
     // Create a GLFWwindow object that we can use for GLFW's functions
     GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr );
     
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
+    
     if ( nullptr == window )
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -46,7 +49,7 @@ int main( )
     }
     
     // Define the viewport dimensions
-    glViewport( 0, 0, WIDTH, HEIGHT );
+    glViewport( 0, 0, screenWidth, screenHeight );
     
     // Game loop
     while ( !glfwWindowShouldClose( window ) )
