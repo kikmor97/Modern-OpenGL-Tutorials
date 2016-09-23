0f110684cc3c51f2e03047eb291ef8b9f3c68ca8
Sonar Systems <SonarSystems@users.noreply.github.com>
2016-09-23 22:52:12 +0100

    Update main.cpp

--- [GETTING STARTED]/[3] Textures/main.cpp
+++ [GETTING STARTED]/[3] Textures/main.cpp
@@ -41,7 +41,7 @@ int main( )
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate( );
         
-        return -1;
+        return EXIT_FAILURE;
     }
     
     glfwMakeContextCurrent( window );
@@ -52,7 +52,7 @@ int main( )
     if ( GLEW_OK != glewInit( ) )
     {
         std::cout << "Failed to initialize GLEW" << std::endl;
-        return -1;
+        return EXIT_FAILURE;
     }
     
     // Define the viewport dimensions
@@ -162,7 +162,7 @@ int main( )
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate( );
     
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
